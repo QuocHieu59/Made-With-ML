82ba6d41d263b15ccccd6f61e61109a58eaeb289
QuocHieu59 <hieu.nq204746@sis.hust.edu.vn>
2024-12-27 14:53:12 +0700

    fix bug v1

--- madewithml/config.py
+++ madewithml/config.py
@@ -10,8 +10,11 @@ import mlflow
 ROOT_DIR = Path(__file__).parent.parent.absolute()
 LOGS_DIR = Path(ROOT_DIR, "logs")
 LOGS_DIR.mkdir(parents=True, exist_ok=True)
+DEFAULT_DIR = Path(os.environ.get("GITHUB_WORKSPACE", "/tmp"), "shared_storage")
 EFS_DIR = Path(f"/efs/shared_storage/madewithml/{os.environ.get('GITHUB_USERNAME', '')}")
-EFS_DIR = ROOT_DIR / EFS_DIR.relative_to(EFS_DIR.anchor)
+EFS_DIR = ROOT_DIR / EFS_DIR.relative_to(EFS_DIR.anchor) if EFS_DIR.exists() else DEFAULT_DIR
+EFS_DIR.mkdir(parents=True, exist_ok=True)
+
 try:
     Path(EFS_DIR).mkdir(parents=True, exist_ok=True)
 except OSError:
